e4b04199de478c5480e46c977be9f65ab97f63bb
Pablo <pablo.rufat@betrybe.com>
2022-03-25 14:14:05 -0300

    feat: fix router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { UserController } from '../controllers';
 
 const router = Router();
 
 const userController: UserController = new UserController();
 
-router.get('/', userController.exampleMethod);
+router.get('/', async (req: Request, res: Response) => await userController.exampleMethod(req, res));
 
 export default router;
